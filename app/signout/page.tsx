1fa07accda5e01dc5e08eaaa9f14e4424a39380c
pedromartins <pedromartins@vintglobal.com.br>
2026-03-17 14:24:03 -0400

    tela de sair

---
+++ app/signout/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { signOut } from "next-auth/react";
+import { useEffect } from "react";
+
+export default function SignOutPage() {
+  useEffect(() => {
+    signOut({ callbackUrl: "/auth" });
+  }, []);
+  return (
+    <div>
+      ...
+    </div>
+  );
+}
